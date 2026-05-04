1cd5b0bfef56c5df6de30208de8a0ff213d3f759
Arjun Menon <am9755@neuronic.cs.princeton.edu>
2026-05-04 10:38:19 -0400

    updates

---
+++ het_control/run_scripts/run_sampling_iddpg_cadico.py
@@ -0,0 +1,11 @@
+import hydra
+from omegaconf import DictConfig
+from het_control.run import get_experiment
+
+@hydra.main(version_base=None, config_path="../conf", config_name="sampling_iddpg_cadico_config")
+def hydra_experiment(cfg: DictConfig) -> None:
+    experiment = get_experiment(cfg=cfg)
+    experiment.run()
+
+if __name__ == "__main__":
+    hydra_experiment()
